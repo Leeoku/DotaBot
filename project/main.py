fbd0e52eaf4f019a5523e3123de454b5633b1a5e
Leeoku <leeoku@gmail.com>
2020-05-19 01:55:50 -0400

    Add filter hero logic, need to fix for multiple case

--- project/main.py
+++ project/main.py
@@ -1,15 +1,25 @@
-import discord, os
+import discord, os, operator
 from discord.ext import commands
 from heroes import heroes
 
-# def compare(self):
-#     if self not in heroes:
-#         print(f"{self} doesnt exist.")
-#     else:
-#         pass
+def filter_hero(hero_name):
+    filtered_heroes = []
+    for e in heroes:
+        if hero_name in e:
+            filtered_heroes.append(e)
+    if len(filtered_heroes) == 1:
+        return filtered_heroes[0]
+    elif len(filtered_heroes) == 0:
+        return False
+    else:
+        return ["multiple", filtered_heroes]
 
 def is_valid_hero(hero_name):
-    return hero_name in heroes
+    #return (hero_name in heroes)
+    if any(hero_name in s for s in heroes):
+        return True
+    else:
+        return False
 
 bot = commands.Bot(command_prefix = '!')
 
@@ -18,14 +28,29 @@ async def on_ready():
     print(f'{bot.user} is ready!')
 
 @bot.command()
-async def hero(ctx, *, arg):
-    dotabuff_hero_name = arg.replace(" ", "-").lower()
-    print(dotabuff_hero_name)
-    dotawiki_hero_name = arg.replace(" ","_").title()
-    print(dotawiki_hero_name)
-    dotabuff_link = f"https://www.dotabuff.com/heroes/{dotabuff_hero_name}"
-    dotawiki_link = f"https://dota2.gamepedia.com/{dotawiki_hero_name}"
-    await ctx.send(f'Dotabuff: {dotabuff_link}\n DotaWiki: {dotawiki_link}')
+async def hero(ctx, *, arg=None):
+    if arg != None:
+        arg = arg.lower()
+    else:
+        pass
+    if not arg:
+        await ctx.send(f"Please enter hero!")    
+    elif arg == None:
+        await ctx.send(f"Hero doesn't exist!")  
+    elif is_valid_hero(arg) == True and filter_hero(arg) == "multiple":
+        print(filter_hero(arg))  
+    elif is_valid_hero(arg) == True:      
+        filter_hero_name = filter_hero(arg)
+        dotabuff_hero_name = filter_hero_name.replace(" ", "-").lower()
+        print(dotabuff_hero_name)
+        dotawiki_hero_name = filter_hero_name.replace(" ","_").title()
+        print(dotawiki_hero_name)
+        dotabuff_link = f"https://www.dotabuff.com/heroes/{dotabuff_hero_name}"
+        dotawiki_link = f"https://dota2.gamepedia.com/{dotawiki_hero_name}"
+        await ctx.send(f'Dotabuff: {dotabuff_link}\n DotaWiki: {dotawiki_link}')
+    else:
+        #print(filtered)
+        await ctx.send(f"Please refine your search!")
 
 if __name__ == '__main__':
     bot.run(os.environ.get("DISCORD_SECRET_KEY"))
--- project/test_main.py
+++ project/test_main.py
@@ -4,10 +4,10 @@ from main import is_valid_hero
 
 @pytest.mark.parametrize("hero_name, expected", [
     ("JET", False),
-    (1, False),
+    ("1", False),
     ("techies", True),
     ("legion commander", True),
-    ("", False)
+    #("", False)
 ])
 def test_valid_hero(hero_name, expected):
     assert is_valid_hero(hero_name) is expected
