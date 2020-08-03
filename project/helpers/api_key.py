d31d93074af43ca7fe00dc82b55df35c5d6219a2
Leeoku <leeoku@gmail.com>
2020-08-03 12:10:37 -0400

    added template apikey

---
+++ project/helpers/api_key.py
@@ -0,0 +1,11 @@
+import pymongo
+import asyncio
+from pymongo import MongoClient
+from motor.motor_asyncio import AsyncIOMotorClient
+import uvloop
+
+asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
+
+discord_key = "YOUR DISCORD KEY HERE"
+cluster = AsyncIOMotorClient('YOUR MONGO CONNECTION HERE')
+
